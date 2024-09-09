5c8c13843f76a75c312f116e7ec706c2c0c3aad5
Jbeli Walid <108816768+Walidij216-tech@users.noreply.github.com>
2024-09-09 19:56:38 +0200

    Delete nsumoapp/controllers/NsumoController/sumo_robot directory

--- nsumoapp/controllers/NsumoController/sumo_robot/src/common/sleep.c
+++
@@ -1,8 +0,0 @@
-#include "/home/walid/Downloads/temp_workspace/sumo_robot/bots2d/nsumoapp/controllers/NsumoController/sumo_robot/src/common/sleep.h"
-#include "/home/walid/Downloads/temp_workspace/sumo_robot/bots2d/nsumoapp/controllers/NsumoController/sumo_robot/src/common/defines.h"
-
-void sleep_ms(uint32_t ms)
-{
-    // Do nothing
-    UNUSED(ms);
-}
